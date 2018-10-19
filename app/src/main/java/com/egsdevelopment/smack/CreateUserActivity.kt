906b7c4d758c8b311a501a49e9d281de0e219c5b
evertsmits <egsdevelopmentmail@gmail.com>
2018-10-19 19:53:20 +0200

    added createUser UI

---
+++ app/src/main/java/com/egsdevelopment/smack/CreateUserActivity.kt
@@ -0,0 +1,25 @@
+package com.egsdevelopment.smack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun generateUserAvatar(view: View) {
+
+    }
+
+    fun createUserClicked(view: View) {
+
+    }
+
+    fun generateColorClicked(view: View) {
+
+    }
+}
