bdb91395bcdeabd5f6dc9f9b75638afffdbf0d3a
evertsmits <egsdevelopmentmail@gmail.com>
2018-10-20 20:52:01 +0200

    polishing login

--- app/src/main/java/com/egsdevelopment/smack/Controllers/LoginAcitivity.kt
+++ app/src/main/java/com/egsdevelopment/smack/Controllers/LoginAcitivity.kt
@@ -1,11 +1,15 @@
 package com.egsdevelopment.smack.Controllers
 
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import com.egsdevelopment.smack.R
 import com.egsdevelopment.smack.Services.AuthService
+import kotlinx.android.synthetic.main.activity_create_user.*
 import kotlinx.android.synthetic.main.activity_login_acitivity.*
 
 class LoginAcitivity : AppCompatActivity() {
@@ -13,29 +17,66 @@ class LoginAcitivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_acitivity)
+        loginSpinner.visibility = View.INVISIBLE
     }
 
     fun loginLoginBtnClicked(view: View) {
+        loginSpinner(true)
         val email = loginEmailText.text.toString()
         val password = loginPasswordText.text.toString()
-
-        AuthService.loginUser(this, email, password) { loginSucces ->
-            println("DIE DINGETJE")
-            println("DEM TOKEN" + AuthService.authToken)
-            if (loginSucces) {
-                println("CALLING FINDUSERBYEMAIL")
-                AuthService.findUserByEmail(this) { findSuccess ->
+        hideKeyBoard()
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            AuthService.loginUser(this, email, password) { loginSucces ->
+                println("DIE DINGETJE")
+                println("DEM TOKEN" + AuthService.authToken)
+                if (loginSucces) {
+                    println("CALLING FINDUSERBYEMAIL")
+                    AuthService.findUserByEmail(this) { findSuccess ->
                         if (findSuccess) {
                             finish()
+                        } else {
+                            errorToast()
                         }
                     }
+                } else {
+                    errorToast()
                 }
             }
+        } else {
+            fillInCredentialsToast()
+        }
         }
 
     fun loginCreateUserBtnClicked(view: View) {
         val createUserIntent = Intent(this, CreateUserActivity::class.java)
         startActivity(createUserIntent)
         finish()
+    }
+
+    fun errorToast() {
+        loginSpinner(false)
+        Toast.makeText(this, "Something went wrong, please try again", Toast.LENGTH_LONG).show()
+    }
+
+    fun fillInCredentialsToast() {
+        loginSpinner(false)
+        Toast.makeText(this, "Please fill in all the credentials", Toast.LENGTH_SHORT).show()
+    }
+
+    fun loginSpinner(enable: Boolean) {
+        if (enable) {
+            loginSpinner.visibility = View.VISIBLE
+        } else {
+            loginSpinner.visibility = View.INVISIBLE
+        }
+        loginLoginBtn.isEnabled = !enable
+        loginCreateUserBtn.isEnabled = !enable
+    }
+
+    fun hideKeyBoard() {
+        val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        if (inputManager.isAcceptingText) {
+            inputManager.hideSoftInputFromWindow(currentFocus.windowToken, 0)
+        }
     }
 }
