d39ff65ea398d0b77cd5ad0f5b86c4c320a22053
evertsmits <egsdevelopmentmail@gmail.com>
2018-10-20 17:57:11 +0200

    Added some UX improvements

--- app/src/main/java/com/egsdevelopment/smack/Controllers/MainActivity.kt
+++ app/src/main/java/com/egsdevelopment/smack/Controllers/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.egsdevelopment.smack.Controllers
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.Color
 import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.egsdevelopment.smack.R
+import com.egsdevelopment.smack.Services.AuthService
+import com.egsdevelopment.smack.Services.UserDataService
+import com.egsdevelopment.smack.Utilities.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.nav_header_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,6 +34,22 @@ class MainActivity : AppCompatActivity() {
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
 
+        LocalBroadcastManager.getInstance(this).registerReceiver(userDataChangeReceiver, IntentFilter(
+            BROADCAST_USER_DATA_CHANGE))
+
+    }
+
+    private val userDataChangeReceiver = object: BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (AuthService.isLoggedIn) {
+                userNameNavHeader.text = UserDataService.name
+                userEmailNavHeader.text = UserDataService.email
+                val resourceId = resources.getIdentifier(UserDataService.avatarName, "drawable", packageName)
+                userImageNavHeader.setImageResource(resourceId)
+                userImageNavHeader.setBackgroundColor(UserDataService.returnAvatarColor(UserDataService.avatarColor))
+                loginBtnNavHeader.text = "Logout"
+            }
+        }
     }
 
     override fun onBackPressed() {
@@ -36,8 +61,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun loginBtnNavClicked(view: View) {
-        val loginIntent = Intent(this, LoginAcitivity::class.java)
-        startActivity(loginIntent)
+        if (AuthService.isLoggedIn) {
+            //logout
+            UserDataService.logout()
+            userNameNavHeader.text = "..."
+            userEmailNavHeader.text = "..."
+            userImageNavHeader.setImageResource(R.drawable.profiledefault)
+            userImageNavHeader.setBackgroundColor(Color.TRANSPARENT)
+            loginBtnNavHeader.text = "login"
+        } else {
+            val loginIntent = Intent(this, LoginAcitivity::class.java)
+            startActivity(loginIntent)
+        }
     }
 
     fun addChannelClicked(view: View) {
--- app/src/main/java/com/egsdevelopment/smack/Services/UserDataService.kt
+++ app/src/main/java/com/egsdevelopment/smack/Services/UserDataService.kt
@@ -1,5 +1,8 @@
 package com.egsdevelopment.smack.Services
 
+import android.graphics.Color
+import java.util.*
+
 object UserDataService {
 
     var id = ""
@@ -9,6 +12,39 @@ object UserDataService {
     var name = ""
 
 
+    fun returnAvatarColor(components: String) : Int{
+        val strippedColor = components
+            .replace("[", "")
+            .replace("]", "")
+            .replace(",", "")
 
+        var r = 0
+        var g = 0
+        var b = 0
+
+        println("Strippedcolor: ${strippedColor}")
+
+        val scanner = Scanner(strippedColor).useLocale(Locale.US)
+
+        if (scanner.hasNext()) {
+            r = (scanner.nextDouble() * 255).toInt()
+            g = (scanner.nextDouble() * 255).toInt()
+            b = (scanner.nextDouble() * 255).toInt()
+        }
+
+        return Color.rgb(r,g,b)
+
+    }
+
+    fun logout() {
+        id = ""
+        avatarColor = ""
+        avatarName = ""
+        email = ""
+        name = ""
+        AuthService.authToken = ""
+        AuthService.userEmail = ""
+        AuthService.isLoggedIn = false
+    }
 
 }
